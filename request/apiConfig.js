b9c2008966e81411521b7ea2b4cba01648d80d00
monish <mohnish.barse@pixonix.tech>
2024-03-24 00:06:32 +0530

    config change

--- request/apiConfig.js
+++ request/apiConfig.js
@@ -1,3 +1,3 @@
-// const MAIN_URL = "https://manufacturingmanagementbackend.onrender.com/api"
-const MAIN_URL = "http://localhost:8000/api"
+const MAIN_URL = "https://manufacturingmanagementbackend.onrender.com/api"
+// const MAIN_URL = "http://localhost:8000/api"
 export default MAIN_URL
